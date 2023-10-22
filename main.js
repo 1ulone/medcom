c1cb911dc6a095a1b4fe2a61e34beeee4f2784a4
ulone <42242738+1ulone@users.noreply.github.com>
2023-10-22 08:13:41 +0100

    better

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-var onSidebar;
+let slideIndex = 1;
+let onauto;
+var timeout;
+//showSlide();
 
-function toggleSidebar() {
-	if (onSidebar)
-		document.getElementById("sidebar").style.width = "0";
-	else
-		document.getElementById("sidebar").style.width = "250px";
+function nextSlide(n) 
+{
+	onauto = true;
+	slideIndex += 1;
+	clearTimeout(timeout);
+	var re = setTimeout(function() 
+		{
+			onauto = false;
+			clearTimeout(re); 
+			showSlide();
+		}, 30000);
+	showSlide();
+}
 
-	onSidebar = !onSidebar;
+function currentSlide(n) 
+{
+}
+
+function showSlide()
+{
+	let i;
+	let slides = document.getElementsByClassName("c");
+	
+	if (slideIndex < 1) { slideIndex = slides.length; }
+
+	for (i = 0; i < slides.length; i++)
+		{ slides[i].style.display = "none"; }
+
+	if (!onauto)
+		slideIndex++;
+
+	if (slideIndex > slides.length) { slideIndex = 1; }
+	slides[slideIndex-1].style.display="block";
+	if (!onauto)
+		timeout = setTimeout(showSlide, 30000);
+
 }
