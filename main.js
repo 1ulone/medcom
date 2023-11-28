90dbb5655b5cb77839d1d391f7d37faea523408c
ulone <42242738+1ulone@users.noreply.github.com>
2023-11-28 18:05:16 +0000

    up

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
 let slideIndex = 1;
+let onsearch;
 let onauto;
 var timeout;
+
+const nxtButtonP = document.getElementById("next");
+const prvbButtonP = document.getElementById("prev");
 //showSlide();
+//
+//
+
+setInterval(()=> { 
+	if (onsearch)
+	{
+		if (document.querySelector(".main-container:hover") == null) 
+		{
+			document.getElementById("mb").style.display = "inline";
+			document.getElementById("search").style.display = "none";
+			onsearch = false;
+		}
+	}
+}, 10);
+
+	let scdiv = document.getElementsByClassName("item");
+
+	scdiv.forEach((it, a) =>
+	{
+		let itemDimension = it.getBoundingClientRect();
+		let itemWidth = itemDimension.width;
+
+		nxtButtonP.addEventListener('click', ()=> 
+		{
+			it.scrollLeft += itemWidth;
+		});
+
+		prvButtonP.addEventListener('click', ()=> 
+		{
+			it.scrollLeft -= itemWidth;
+		});
+
+	});
+
+function showsearch()
+{
+	if (!onsearch)
+	{
+		document.getElementById("mb").style.display = "none";
+		document.getElementById("search").style.display = "inline";
+		onsearch = true;
+	}
+}
 
 function nextSlide(n) 
 {
